31020cd4d5a881854aee2020a7a20e6e59b1bd7b
RaghavAggarwal <raghavaggarwal3110@gmail.com>
2026-02-11 12:52:13 +0530

    practice ques2

---
+++ ex2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int j=0;j<n/2;j++){
+        int temp=arr[j];
+        arr[j]=arr[n-1-j];
+        arr[n-1-j]=temp;
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+
+}
